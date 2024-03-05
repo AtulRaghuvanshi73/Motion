e8ecbf98326bf7feb8a5ac1de4fd85cd666bef96
AtulRaghuvanshi73 <atulraghuvanshi09@gmail.com>
2024-03-06 02:07:17 +0530

    feat: implemented auth

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
-
+import { Spinner } from "@/components/ui/spinner";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import { ArrowRight} from "lucide-react";
+import Link from "next/link";
 export const Heading = () => {
+
+    const {isAuthenticated, isLoading} = useConvexAuth();
+
     return(
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -12,10 +18,28 @@ export const Heading = () => {
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
                 Motion is a place to come together as a team and work on things which matter!
             </h3>
-            <Button>
-                Get in Motion
-                <ArrowRight className="h-4 w-4 ml-2" />
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size ="lg"/>
+                </div>
+            )}
+            { isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                       Enter Motion
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Link>
+                </Button>
+            )}
+
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        Get Motion Free
+                        <ArrowRight className="h-4 w-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     );
 }
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { useScrollTop } from "@/hooks/use-scroll-top";
+import {SignInButton, UserButton} from "@clerk/clerk-react";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
+import Link from 'next/link'
 
 export const Navbar = () => {
-
+    const { isAuthenticated, isLoading} = useConvexAuth();
     const scrolled = useScrollTop();
 
     return(
@@ -16,6 +21,39 @@ export const Navbar = () => {
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+
+                {isLoading && (
+                    <Spinner />
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="ghost" size="sm">
+                                Log in
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm">
+                                Get a free Version
+                            </Button>
+                        </SignInButton>
+                    
+                    </>
+                )
+
+                }
+                {isAuthenticated && !isLoading &&(
+                    <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">
+                                Enter Motion
+                            </Link>
+                        </Button>
+                        <UserButton 
+                          afterSignOutUrl="/"
+                        />
+                    </>
+                )}
                 <ModeToggle />
             </div>
         </div>
