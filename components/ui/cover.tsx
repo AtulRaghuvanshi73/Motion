4bc5e4654402173e1467c5e035792855748c3bfd
ar6625 <ar6625@srmist.edu.in>
2024-04-15 08:22:50 +0530

    feat: added deletion and replacement of cover img

--- components/ui/cover.tsx
+++ components/ui/cover.tsx
@@ -5,7 +5,11 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ImageIcon, X } from "lucide-react";
 import { useCoverImage } from "@/hooks/use-cover-image";
-
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api"
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverImageProps {
     url?:string;
@@ -17,7 +21,22 @@ export const Cover = ({
     preview,
 }: CoverImageProps) => {
 
+    const { edgestore } = useEdgeStore();
+    const params = useParams();
     const coverImage = useCoverImage();
+    const removeCoverImage = useMutation(api.documents.removeCoverImage);
+
+
+    const onRemove = async () => {
+        if(url){
+                await edgestore.publicFiles.delete({
+                url: url
+            })
+        }
+        removeCoverImage({
+            id: params.documentId as Id<"documents">   
+        });
+    }
 
     return (
         <div className={cn(
@@ -36,7 +55,7 @@ export const Cover = ({
             {url && !preview && (
                 <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
                     <Button
-                        onClick={coverImage.onOpen}
+                        onClick={() => coverImage.onReplace(url)}
                         className="text-muted-foreground text-xs"
                         variant="outline"
                         size="sm"
@@ -45,7 +64,7 @@ export const Cover = ({
                         Change cover
                     </Button>
                     <Button
-                        onClick={() => {}}
+                        onClick={onRemove}
                         className="text-muted-foreground text-xs"
                         variant="outline"
                         size="sm"
